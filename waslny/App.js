44f72b8aa62de451d0902ab6af97e77f7d655ce5
maher malhem <maher.malhem2@gmail.com>
2022-12-29 02:20:14 +0300

    Init

---
+++ waslny/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+
+
+  return (
+    <SafeAreaView>
+      <Text>helloo</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
